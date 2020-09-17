aef3abe8c2ee4ad9f2e58e30921fef6c657dcdfe
Jennifer Jacobson <jenniferjacobson@Jennifers-MacBook-Air.local>
2020-09-17 11:33:08 -0500

    fixed pilot name alert and planet load time

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json){
+         const div = document.getElementById("missionTarget");
+         div.innerHTML =`
+         <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[2].name}</li>
+               <li>Diameter: ${json[2].diameter}</li>
+               <li>Star: ${json[2].star}</li>
+               <li>Distance from Earth: ${json[2].distance}</li>
+               <li>Number of Moons: ${json[2].moons}</li>
+            </ol>
+            <img src="${json[2].image}">   
+         `
+      });
+   });
    let form = document.getElementById("launchForm");
    form.addEventListener("submit", function(event) {
       let pilotNameInput = document.getElementById("pilotName");
@@ -11,25 +27,12 @@ window.addEventListener("load", function() {
       let fuelStatus = document.getElementById("fuelStatus");
       let cargoStatus = document.getElementById("cargoStatus");
       let launchStatus= document.getElementById("launchStatus");
-      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-         response.json().then(function(json){
-            const div = document.getElementById("missionTarget");
-            div.innerHTML =`
-            <h2>Mission Destination</h2>
-               <ol>
-                  <li>Name: ${json[2].name}</li>
-                  <li>Diameter: ${json[2].diameter}</li>
-                  <li>Star: ${json[2].star}</li>
-                  <li>Distance from Earth: ${json[2].distance}</li>
-                  <li>Number of Moons: ${json[2].moons}</li>
-               </ol>
-               <img src="${json[2].image}">   
-            `
-         });
-      });
+
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || isNaN(fuelLevelInput.value) === true || cargoMassInput.value === "" || isNaN(cargoMassInput.value) === true){
          alert("All fields required");
          event.preventDefault();
+      }else if (isNaN(pilotNameInput.value) !== true || isNaN(copilotNameInput.value) !== true) {
+         alert("Pilot name invalid");
       }else{
          event.preventDefault();
          pilotStatus.style.visibility = `visible`
